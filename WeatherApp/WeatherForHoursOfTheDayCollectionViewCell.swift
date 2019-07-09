93d6c78935143f2114d6ac2581faa638b433f4c1
Ezgi Erden <ezgrden@gmail.com>
2019-07-09 15:59:05 +0300

    Added collection view basics

---
+++ WeatherApp/WeatherForHoursOfTheDayCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForHoursOfTheDayCollectionViewCell.swift
+//  WeatherApp
+//
+//  Created by Ezgi Erden on 09/07/2019.
+//  Copyright © 2019 Ezgi Erden. All rights reserved.
+//
+
+import UIKit
+
+class WeatherForHoursOfTheDayCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var iconCellImageView: UIImageView!
+    @IBOutlet weak var timeCellLabel: UILabel!
+    @IBOutlet weak var degreeCellLabel: UILabel!
+    
+}
