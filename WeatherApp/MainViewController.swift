6efb2a87d66210485bc79dc695dc0464ca47d156
Ezgi Erden <ezgrden@gmail.com>
2019-07-15 16:40:04 +0100

    Collection view with assets (not working)

--- WeatherApp/MainViewController.swift
+++ WeatherApp/MainViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    var weatherIcon = [UIImage(named: "cloudy"), UIImage(named: "few clouds & sun")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+ func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return weatherIcon.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "", for: indexPath) as! WeatherForHoursOfTheDayCollectionViewCell
+        
+        cell.iconCellImageView.image = weatherIcon[indexPath.row]
+        
+        return cell
     }
 
 
