bce15c7a04a6a121e37a8bdc3ef1e76435770507
Ezgi Erden <ezgrden@gmail.com>
2019-07-23 22:25:19 +0100

    hourlyForecasts are added

--- WeatherApp/MainViewController.swift
+++ WeatherApp/MainViewController.swift
@@ -11,30 +11,37 @@ import UIKit
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     @IBOutlet weak var collView: UICollectionView!
-    var weatherArray: [Weather] = []
+    var dailyWeatherArray: [Weather] = []
+    var hourlyWeatherArray: [Weather] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        Weather.forecast(withLocation: "42.3601,-71.0589") { (results:[Weather]) in
-            self.weatherArray = results
+        Weather.forecast(withLocation: "42.3601,-71.0589") { (dailyResults:[Weather], hourlyResults:[Weather]) in
+            self.dailyWeatherArray = dailyResults
+            self.hourlyWeatherArray = Array(hourlyResults[0...23])
             self.collView?.reloadData()
-            //for result in results {
-            //    print("\(result.temperatureMax)\n\n")
-            //}
         }
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return weatherArray.count
+        return hourlyWeatherArray.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WeatherForHoursOfTheDayCollectionViewCell", for: indexPath) as! WeatherForHoursOfTheDayCollectionViewCell
         
-        cell.degreeCellLabel.text = String(weatherArray[indexPath.row].temperatureMax)
+        cell.degreeCellLabel.text = String(hourlyWeatherArray[indexPath.row].temperature!)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+        dateFormatter.dateFormat = "HH"
+        let time = dateFormatter.string(from: hourlyWeatherArray[indexPath.row].time)
+        
+        cell.timeCellLabel.text = time
+        
         return cell
     }
 
