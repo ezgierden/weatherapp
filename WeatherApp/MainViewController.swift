1aee8d790bfa792c38bc1c621d5536bcc214cf3c
Ezgi Erden <ezgrden@gmail.com>
2019-08-05 00:39:21 +0100

    Background image and lines are added to the main screen

--- WeatherApp/MainViewController.swift
+++ WeatherApp/MainViewController.swift
@@ -15,16 +15,37 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     @IBOutlet weak var dateAndTimeLabel: UILabel!
     @IBOutlet weak var summaryLabel: UILabel!
     @IBOutlet weak var degreeLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var windSpeedLabel: UILabel!
+    @IBOutlet weak var cloudCoverLabel: UILabel!
+    @IBOutlet weak var visibilityLabel: UILabel!
+    @IBOutlet weak var backgroundImageView: UIImageView!
     
     @IBOutlet weak var collView: UICollectionView!
     
     var dailyWeatherArray: [Weather] = []
     var hourlyWeatherArray: [Weather] = []
     var currentWeatherArray: [String:Any] = [:]
- 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let collectionViewFirstline = UIView(frame: CGRect(x: 10, y: 493, width: 400, height: 1.0))
+        collectionViewFirstline.layer.borderWidth = 1.0
+        collectionViewFirstline.layer.borderColor = UIColor.white.cgColor
+        self.view.addSubview(collectionViewFirstline)
+        
+        let collectionViewSecondLine = UIView(frame: CGRect(x: 10, y: 628, width: 400, height: 1.0))
+        collectionViewSecondLine.layer.borderWidth = 1.0
+        collectionViewSecondLine.layer.borderColor = UIColor.white.cgColor
+        self.view.addSubview(collectionViewSecondLine)
+        
+        let bottomLine = UIView(frame: CGRect(x: 10, y: 780, width: 400, height: 1.0))
+        bottomLine.layer.borderWidth = 1.0
+        bottomLine.layer.borderColor = UIColor.white.cgColor
+        self.view.addSubview(bottomLine)
+        
         
         Weather.forecast(withLocation: "42.3601,-71.0589") { (darkSkyApiResponse:DarkSkyApiResponse) in
             
@@ -33,9 +54,15 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             self.currentWeatherArray = darkSkyApiResponse.currentList
             
             DispatchQueue.main.async {
-                self.highestTempOfTheDayLabel.text = String(darkSkyApiResponse.dailyList[0].temperatureMax!)
-                self.lowestTempOfTheDayLabel.text = String(darkSkyApiResponse.dailyList[0].temperatureMin!)
-                self.summaryLabel.text = darkSkyApiResponse.currentList["summary"] as! String
+                self.highestTempOfTheDayLabel.text = String(Int(darkSkyApiResponse.dailyList[0].temperatureMax!))
+                self.lowestTempOfTheDayLabel.text = String(Int(darkSkyApiResponse.dailyList[0].temperatureMin!))
+                self.summaryLabel.text = darkSkyApiResponse.currentList["summary"] as? String
+                self.degreeLabel.text = String(Int(darkSkyApiResponse.currentList["temperature"] as! Double)) + "°"
+                self.humidityLabel.text = String(darkSkyApiResponse.currentList["humidity"] as! Double)
+                self.windSpeedLabel.text = String(darkSkyApiResponse.currentList["windSpeed"] as! Double)
+                self.cloudCoverLabel.text = String(darkSkyApiResponse.currentList["cloudCover"] as! Double)
+                self.visibilityLabel.text = String(darkSkyApiResponse.currentList["visibility"] as! Double)
+                self.backgroundImageView.image = UIImage(named: (darkSkyApiResponse.currentList["icon"] as! String) + "BG")
                 
                 let dateFormatter = DateFormatter()
                 dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
